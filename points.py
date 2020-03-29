f11467ccd2ef33c74fefa044d4cf9560a6874f25
Alexander Patel <acpatel@andrew.cmu.edu>
2020-03-28 21:25:50 -0400

    converting screen space pixels to world space

--- points.py
+++ points.py
@@ -4,12 +4,16 @@ import numpy as np
 from skimage import io, filters, feature, draw
 import matplotlib.pyplot as plt
 
-## SCAN CONSTANTS ##
+## SCAN CONSTANTS (all numbers in meters) ##
 
 # taken from blender object properties
-CAMERA_POS = np.array([[ 0.00], [7.19], [3.79], [1.00]])
 LASER_POS  = np.array([[-2.50], [7.19], [3.79], [1.00]])
 TARGET_POS = np.array([[ 0.00], [0.00], [0.50], [1.00]])
+
+CAMERA_POS           = np.array([[ 0.00], [7.19], [3.79], [1.00]])
+CAMERA_FOCAL_LENGTH  = 0.050
+CAMERA_SENSOR_WIDTH  = 0.036
+CAMERA_SENSOR_HEIGHT = 0.024
 
 ############################################
 ## TAKEN FROM BLENDER SCRIPT OF THE FORM: ##
@@ -25,30 +29,49 @@ CAMERA_TO_WORLD = np.linalg.inv(np.array(
    [ 0.0000,  0.9093, 0.4161, 3.7900],
    [ 0.0000,  0.0000, 0.0000, 1.0000]]))
 
-def pixels_to_screen_points(pixels):
-  pass
-
-def detect_laser_pixels(image):
+# detects pixels along the laser line in the image
+# RETURNS: list of laser pixels as (x, y) tuples
+def detect_laser_pixels(image, dim):
   # extract element-wise channel diff
-  image[...,1] >>= 1
-  image[...,2] >>= 1
+  image[...,1] >>= 1; image[...,2] >>= 1
   diff_image = image[...,0] - image[...,1] - image[...,2]
 
-  # compute row-wise highest intensity pixel
+  # compute row-wise highest intensity pixels (max 3 per row)
   threshold = 100 # this is an arbitrary number
   pixels = []
-  for row in range(image.shape[0]):
+  for row in range(dim[0]):
     coordinates = \
-      feature.peak.peak_local_max(diff_image[row,:], threshold_abs=threshold,
-                                  num_peaks=3)
+      feature.peak.peak_local_max(diff_image[row,:],
+        threshold_abs=threshold, num_peaks=3)
     for coordinate in coordinates:
-      pixels.append((row,coordinate[0]))
-  
+      pixels.append((coordinate[0], row))
   return pixels
 
-def pixels_to_screen_points(pixels):
-  pass
+# converts a list of pixels to corresponding screen points
+# in world space as a numpy array
+# RETURNS: (#pixels)x3 numpy array of screen points
+def pixels_to_screen_points(pixels, dim):
+  iw, ih  = 1.0 / dim[1], 1.0 / dim[0]
+  tx, ty  = iw * CAMERA_SENSOR_WIDTH, ih * CAMERA_SENSOR_HEIGHT
+  ox, oy  = CAMERA_SENSOR_WIDTH * 0.5, CAMERA_SENSOR_HEIGHT * 0.5
+  npixels = len(pixels)
 
+  # convert pixels to camera space
+  camera_points = np.zeros((4, npixels))
+  for i in range(npixels):
+    x, y = pixels[i][0] * tx - ox, pixels[i][1] * ty - oy
+    camera_points[:,i] = np.array([x, y, CAMERA_FOCAL_LENGTH, 1.00])
+
+  # convert pixels to world space
+  screen_points = (CAMERA_TO_WORLD @ camera_points)[0:3,:]
+  return screen_points
+
+# read scan data
 image = io.imread('scan/scan00001.png')
-pixels = detect_laser_pixels(image)
-print(pixels)
+dim   = image.shape
+
+pixels        = detect_laser_pixels(image, dim)
+screen_points = pixels_to_screen_points(pixels, dim)
+print(dim)
+print(pixels[0])
+print(screen_points[:,0])
